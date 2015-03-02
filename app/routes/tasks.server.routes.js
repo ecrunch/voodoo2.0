8306b9df3650064dacf1acd42dbca65945bb4058
zack <zackbotkin@gmail.com>
2015-03-01 20:09:27 -0600

    finished up task routes skeleton

--- app/routes/tasks.server.routes.js
+++ app/routes/tasks.server.routes.js
@@ -4,13 +4,24 @@
  * Module dependencies.
  */
 var users = require('../../app/controllers/users.server.controller'),
-	articles = require('../../app/controllers/articles.server.controller');
+	tasks = require('../../app/controllers/tasks.server.controller');
 
 
 module.exports = function(app) {
 	
 	//Task routes
-	app.route('/tasks');
+	app.route('/tasks')
+		.get(tasks.list)
+		.post(users.requiresLogin, tasks.create);
+
+	app.route('/tasks/:taskId')
+		.get(tasks.read)
+		.put(users.requiresLogin, tasks.hasAuthorization, tasks.update)
+		.delete(users.requiresLogin, tasks.hasAuthorization, tasks.delete);
+
+
+	// Finish by binding the task middleware
+	app.param('taskId', tasks.taskByID);
 	
 };
 
