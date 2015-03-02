6a619b903b2e69d6d98bcf464bce13173e2e6e6e
zack <zackbotkin@gmail.com>
2015-03-01 21:02:02 -0600

    wrote some boiler plate frontend code... at url /schedule

--- public/modules/scheduler/config/scheduler.client.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Configuring the Scheduler module
-angular.module('scheduler').run(['Menus',
-        function(Menus) {
-                // Set top bar menu items
-                Menus.addMenuItem('topbar', 'Scheduler', 'scheduler', '/scheduler');
-        }
-]);
